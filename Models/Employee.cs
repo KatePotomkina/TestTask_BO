e7ca05d90d8106a65669a7ab36d852a55ae5aab7
kate.potomkina <kate.potomkina@gmail.com>
2023-03-15 02:15:06 +0200

    Add sort by name

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -9,7 +9,7 @@ public partial class Employee
 //[Index(0)]
     public int Id { get; set; }
     [Index(0)] public string? Name { get; set; }
-    [Index(1)] public DateTime? DateOfBirth { get; set;}
+    [Index(1)] public DateTime? DateOfBirth { get; set; }
     [Index(2)] public bool Married { get; set; }
     [Index(3)] public string? Phone { get; set; }
     [Index(4)] public decimal? Salary { get; set; }
